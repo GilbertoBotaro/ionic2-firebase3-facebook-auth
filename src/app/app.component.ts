ce93398fd002fe3ea439201a8c7132b6d9601ee0
Jorge Vergara <javebratt@gmail.com>
2016-10-21 07:55:59 -0500

    Updates to RC1 Moves away from Rollout and starts using Webpack

---
+++ src/app/app.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { Platform } from 'ionic-angular';
+import { StatusBar } from 'ionic-native';
+
+import { HomePage } from '../pages/home/home';
+
+import firebase from 'firebase';
+
+
+@Component({
+  template: `<ion-nav [root]="rootPage"></ion-nav>`
+})
+export class MyApp {
+  rootPage = HomePage;
+
+  constructor(platform: Platform) {
+    const firebaseConfig = {
+      apiKey: "AIzaSyALKfevapBOYK202f6k5mPPfMrT1MHDv5A",
+        authDomain: "bill-tracker-e5746.firebaseapp.com",
+        databaseURL: "https://bill-tracker-e5746.firebaseio.com",
+        storageBucket: "bill-tracker-e5746.appspot.com",
+        messagingSenderId: "508248799540"
+    };
+
+    firebase.initializeApp(firebaseConfig);
+    platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      StatusBar.styleDefault();
+    });
+  }
+}
